4c330c12f957e8a67b2288f3be619acbee048721
Everardo Gonzalez <everardo.a.gonzalez@gmail.com>
2020-05-16 23:57:55 -0400

    timing check

--- main.cpp
+++ main.cpp
@@ -4,14 +4,23 @@ int main()
 {
    // initialize game engine
     Game game; //TODO: parameterize the game engine - stuff like window size
+    sf::Clock clock;
 
     while (game.running()) {
 
-            // Update
-            game.update();
+        // Update
+        sf::Int64 game_update_start = clock.getElapsedTime().asMilliseconds();
+        game.update();
+        sf::Int64 game_update_end = clock.getElapsedTime().asMilliseconds();
 
         // Render
+        sf::Int64 game_render_start = clock.getElapsedTime().asMilliseconds();
         game.render();
+        sf::Int64 game_render_end = clock.getElapsedTime().asMilliseconds();
+
+        std::cout << "Update took " << game_update_end - game_update_start << std::endl;
+        std::cout << "Render took " << game_render_end - game_render_start << std::endl;
+        std::cout << "_____________________" << std::endl;
     }
 
     return 0;
